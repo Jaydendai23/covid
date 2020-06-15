cf9345ec281cec087f36f8b5050dc2b3a08e48fb
Jaydendai23 <66967511+Jaydendai23@users.noreply.github.com>
2020-06-15 14:54:44 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+#print ("hello world")
+
+#name = input ("best country in the world:  ")
+#print (name)
+
+'''
+if (name.lower()  == "Canada"):
+     print (name + " is the best!")
+elif (name == "Japan"):
+    print(name.lower() + " is the second best!")
+else:
+    print ("wrong")
+    '''
+
+fruits = ['apple,grannyssmith','orange,mandarin,bloodorange','banana,bigbanana','peach']
+for fruit in fruits:
+    templist = fruit.split (",")
+    print(fruit)
+    print (templist)
+
+
+    
